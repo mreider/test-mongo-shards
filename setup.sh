51e991ffe11efcfdc6d075589f8440c562f49e23
Matthew Reider <matthew.reider@dynatrace.com>
2024-10-16 13:33:37 +0200

    debug -X

--- setup.sh
+++ setup.sh
@@ -7,12 +7,12 @@ sudo apt install -y openjdk-11-jdk maven docker.io docker-compose
 # Build Java services
 echo "Building Service A..."
 cd service-a
-mvn clean package
+mvn clean package -X
 cd ..
 
 echo "Building Service B..."
 cd service-b
-mvn clean package
+mvn clean package -X
 cd ..
 
 # Run Docker Compose
